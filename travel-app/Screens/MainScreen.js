53a56e32e93f2c8475b9ed3eb3356740018025c8
Achyut Patel <137658082+TRshaqOat@users.noreply.github.com>
2023-11-13 14:35:25 -0600

    heyy

--- travel-app/Screens/MainScreen.js
+++ travel-app/Screens/MainScreen.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { View, Text, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const MainScreen = ({ navigation }) => {
+  const [search, setsearch] = useState("");
+  const [data, setData] = useState({});
+
+  const searchFood = async (event) => {
+    const options = {
+      method: "GET",
+      url: "https://travel-info-api.p.rapidapi.com/country",
+      params: { country: "search" },
+      headers: {
+        "X-RapidAPI-Key": "c6d00b02demsh6fbd40f27f235a2p16f75djsn6538ec2274e0",
+        "X-RapidAPI-Host": "travel-info-api.p.rapidapi.com",
+      },
+    };
+    if (event.key === "Enter") {
+      try {
+        try {
+          const response = await axios.request(options);
+          if (response.data.size > 1) {
+            console.log(response.data.length + " Items");
+          }
+
+          setData(response.data[0]);
+          console.log(response.data);
+          console.log("Total: " + newTotalCalories + " Calories");
+        } catch (error) {
+          console.error(error);
+        }
+
+        setFood("");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+      resetAnimation();
+    }
+  };
+
   return (
     <View>
       <Text>Main Screen</Text>
@@ -9,6 +47,14 @@ const MainScreen = ({ navigation }) => {
         title="Go back to Login"
         onPress={() => navigation.navigate("Login")}
       />
+      <TextInput
+        value={search}
+        onChange={(event) => setsearch(event.target.value)}
+        placeholder="Enter Ingredients/Food"
+        type="text"
+      />
+      <Button title="Search" onPress={searchFood} />
+      <Text></Text>
     </View>
   );
 };
